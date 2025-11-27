e57a025e3b44997a0309285cbfb55758e2b8c32e
Najma W Thande <najmathande@gmail.com>
2025-11-27 22:13:35 +0400

    verfiy password function added

--- auth.py
+++ auth.py
@@ -11,3 +11,13 @@ def hash_password(plain_password):
     hashed = bcrypt.hashpw(password_bytes, salt)
 
     return hashed.decode()
+
+# this function is to verify the password during login
+
+def verify_password(plain_password, hashed_password):
+    password_bytes = plain_password.encode()
+    hashed_bytes = hashed_password.encode()
+
+    return bcrypt.checkpw(password_bytes, hashed_bytes)
+
+
